6d05198156c068c4ce723a99675659a530b2fea2
Ivan Budinov <originahurricane@gmail.com>
2021-03-18 13:22:20 +0200

    update menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -41,13 +41,8 @@ function Directory() {
     return(
         <div className='directory-menu'>
             {
-                sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem 
-                    key={id} 
-                    title={title} 
-                    imageUrl={imageUrl} 
-                    size={size}
-                    />
+                sections.map(({id, ...otherSectionProps}) => (
+                    <MenuItem key={id} {...otherSectionProps}/>
                 ))
             }
         </div>
